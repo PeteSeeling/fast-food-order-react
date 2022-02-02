f4f28d039e8910a1538b708d5037f395a324a538
Pete Seeling <peteseeling@gmail.com>
2022-02-01 18:16:20 -0600

    name working properly on form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 
 import './App.css';
+import react from 'react';
+import { useState } from 'react';
+
+import AddName from './order-name';
 
 function App() {
+
+  const [orderName, addOrderName] = useState('Add Your Name Here');
   return (
     <div className="App">
       <div>
-        <h2>Food Order for...</h2>
+        <h2>Food Order for...{orderName}</h2>
       </div>
       <div className='order-form'>
-        
+        <AddName addOrderName={addOrderName} />
+
       </div>
     </div>
   );
---
+++ src/order-name.js
@@ -0,0 +1,11 @@
+import react from 'react';
+
+export default function AddName({ addOrderName }) {
+  function handleChange(e) {
+    addOrderName(e.target.value);
+  }
+  return <div className='add-name-input'>
+      Your Name:
+    <input onChange={handleChange} />
+  </div>;
+}
